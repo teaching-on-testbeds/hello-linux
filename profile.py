7a630912f7c62628b707497f6b508f458f4ec020
Fraida Fund <ffund@nyu.edu>
2023-09-06 11:55:41 -0400

    Use shared VM, Ubuntu 22

--- profile.py
+++ profile.py
@@ -18,9 +18,11 @@ pc = portal.Context()
 request = pc.makeRequestRSpec()
 
 # Set up first host - romeo
-node_romeo = request.RawPC("romeo")
+node_romeo = request.XenVM('romeo')
+node_romeo.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD'
 node_romeo.addService(rspec.Execute(shell="bash", command="/usr/bin/sudo /usr/bin/apt update; /usr/bin/sudo /usr/bin/apt -y install pcmanfm"))
-
+node_romeo.exclusive = False
+node_romeo.routable_control_ip = True # required for VNC
 node_romeo.startVNC()
 
 # Print the RSpec to the enclosing page.
